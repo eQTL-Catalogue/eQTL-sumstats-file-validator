66d66db27ddd8b002e39b2a28d4dc271dbd01498
jdhayhurst <38317975+jdhayhurst@users.noreply.github.com>
2020-04-14 17:32:55 +0100

    Update schema.py
    add X and Y chroms

--- validate/schema.py
+++ validate/schema.py
@@ -11,13 +11,13 @@ VALID_COLS = TO_LOAD_DSET_HEADERS_DEFAULT
 VALID_CHROMOSOMES = ['1', '2', '3', '4', '5', '6', '7', '8', 
                      '9', '10', '11', '12', '13', '14', '15', '16', 
                      '17', '18', '19', '20', '21', '22', '23', '24', 
-                     '25']
+                     '25', 'X', 'Y']
 
 
 VALIDATORS = {
     PVAL_DSET: Column(PVAL_DSET, [CanConvertValidation(DSET_TYPES[PVAL_DSET]), InInclusiveRangeValidation(0, 1)], allow_empty=False),
     BETA_DSET: Column(BETA_DSET, [CanConvertValidation(float)], allow_empty=True),
-    SNP_DSET: Column(SNP_DSET, [CanConvertValidation(DSET_TYPES[SNP_DSET]), MatchesPatternValidation(r'^chr[0-9]+_[0-9]+_[ACTGNactgn]+_[ACTGNactgn]+|LONG_STRING$')], allow_empty=True),
+    SNP_DSET: Column(SNP_DSET, [CanConvertValidation(DSET_TYPES[SNP_DSET]), MatchesPatternValidation(r'^chr[0-9XY]+_[0-9]+_[ACTGNactgn]+_[ACTGNactgn]+|LONG_STRING$')], allow_empty=True),
     CHR_DSET: Column(CHR_DSET, [InListValidation(VALID_CHROMOSOMES)], allow_empty=False),
     BP_DSET: Column(BP_DSET, [CanConvertValidation(DSET_TYPES[BP_DSET]), InInclusiveRangeValidation(1, 999999999)], allow_empty=False),
     EFFECT_DSET: Column(EFFECT_DSET, [MatchesPatternValidation(r'^[ACTGNactgn]+|LONG_STRING$')], allow_empty=True),
